6f6f5ae1ea436da32d29dfa2931fa77e0390537a
Ali <ali62n62@yahoo.com>
2017-07-07 13:29:30 +0430

    adding content to listview

--- ListViewDemo/MainActivity.cs
+++ ListViewDemo/MainActivity.cs
@@ -1,4 +1,5 @@
-﻿using Android.App;
+﻿using System.Collections.Generic;
+using Android.App;
 using Android.Widget;
 using Android.OS;
 
@@ -7,12 +8,25 @@ namespace ListViewDemo
     [Activity(Label = "ListViewDemo", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private ListView _listViewTest;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
-            // SetContentView (Resource.Layout.Main);
+            SetContentView (Resource.Layout.Main);
+            initFields();
+        }
+
+        private void initFields()
+        {
+            _listViewTest = FindViewById<ListView>(Resource.Id.listViewTest);
+            List<string> listMyFamily=new List<string>();
+            listMyFamily.Add("Ali");
+            listMyFamily.Add("Maryam");
+            listMyFamily.Add("Ilia");
+            ArrayAdapter<string> arrayAdapterMyFamily=new ArrayAdapter<string>(this,Android.Resource.Layout.SimpleListItem1,listMyFamily);
+            _listViewTest.Adapter = arrayAdapterMyFamily;
         }
     }
 }
